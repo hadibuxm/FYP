d7f219f120b2fd849620272e1e241a95ec95744d
hadibuxm <mahessarm@gmail.com>
2021-06-06 09:30:12 +0500

    changed ui of comparative analysis page, added table for comparison

--- modules/compareanalysis.py
+++ modules/compareanalysis.py
@@ -16,11 +16,11 @@ def compare(business1, business2, api = API):
     user2 = api.get_user(screen_name = business2)
     business1name = user1.name
     business2name = user2.name
-    business1description = user1.description
-    business2description = user2.description
-    business1followers = user1.followers_count
-    business2followers = user2.followers_count
-    business1url = user1.entities['url']['urls'][0]['expanded_url']
-    business2url = user2.entities['url']['urls'][0]['expanded_url']
+    business1description = 'Description: ' + user1.description
+    business2description = 'Description: ' + user2.description
+    business1followers = 'Followers: ' + str(user1.followers_count)
+    business2followers = 'Followers: ' + str(user2.followers_count)
+    business1url = 'URL: ' + user1.entities['url']['urls'][0]['expanded_url']
+    business2url = 'URL: ' + user2.entities['url']['urls'][0]['expanded_url']
     return business1name, business2name, business1description, business2description, business1followers, business2followers, business1url, business2url
     
