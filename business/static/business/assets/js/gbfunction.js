dc7d6184c733161fc3743cb49f6e700c493799c0
hadibuxm <mahessarm@gmail.com>
2021-06-19 10:41:54 +0500

    used arrays and functions to minimize lines of codes in gbfunctions options

--- business/static/business/assets/js/gbfunction.js
+++ business/static/business/assets/js/gbfunction.js
@@ -1,3 +1,17 @@
+function setoptions(brands, brands_id) {
+  for (let i = 1; i <= brands.length; i++) {
+          document.getElementById(i.toString()).innerHTML = brands[i-1];
+          document.getElementById(i.toString()).value = brands_id[i-1];
+        }
+  if (brands.length != 10){
+    for (let j = brands.length+1; j<=10; j++){
+      document.getElementById(j).style.display = "none";
+    }
+  }
+}
+
+
+
 function hideselectionboxes(){
     document.getElementById("1").style.display= "none";
     document.getElementById("2").style.display= "none";
@@ -15,8 +29,6 @@ function hideselectionboxes(){
     document.getElementById("userid").value = document.getElementById("selectbusiness").value;
   }
 */
-// hide selectionboxes
-  hideselectionboxes();
 
   function showselectionboxes(){
     document.getElementById("1").style.display= "block";
@@ -31,12 +43,32 @@ function hideselectionboxes(){
     document.getElementById("10").style.display= "block";
   }
 
+// hide selectionboxes
+  hideselectionboxes();
   function showbusinesses(){
     // get value of selected category
     var category = document.getElementById("selectcategory").value;
     switch(category){
       case "clothing":
         showselectionboxes(); // show selection boxes
+        const clothing_brands = [
+          "J. Junaid Jamshed" , "Gul Ahmed" , "Nishat Linen", "Bareeze" , "Khaadi" , 
+          "Kayseria", "KROSSKULTURE", "edenrobe", "OrientTextiles", "affordablepk"
+        ];
+
+        const clothing_brands_ids = [
+          "@_JunaidJamshed" , "@GulahmedFashion" , "@nishatlinen","@BareezeOfficial" , 
+          "@KhaadiOfficial" , "@KayseriaPK", "@KrossKulturePk" , "@edenrobe_pk" , 
+          "@OrientTextiles", "@affordablepk"
+        ];
+        setoptions(clothing_brands, clothing_brands_ids);
+        break;
+        /*
+        for (let i = 1; i <= clothing_brands.length; i++) {
+          document.getElementById(i.toString()).innerHTML = clothing_brands[i-1];
+          document.getElementById(i.toString()).value = clothing_brands_ids[i-1];
+        }*/
+        /*
         document.getElementById("1").innerHTML = "J. Junaid Jamshed";
         document.getElementById("1").value = "@_JunaidJamshed";
         document.getElementById("2").innerHTML = "Gul Ahmed";
@@ -58,9 +90,29 @@ function hideselectionboxes(){
         document.getElementById("10").innerHTML = "affordablepk";
         document.getElementById("10").value = "@affordablepk";
         break;
+        */
 
       case "foodonline":
-        showselectionboxes(); 
+        showselectionboxes(); // show selection boxes
+        const food_brands = ["foodpanda" , "kfcpakistan" , "MENU", 
+                                "Eat Mubarak" , "Supermeal Pakistan" , "K&N's Pakistan", "foodpakistan", 
+                                "foodnerd", "cheetay"
+                            ];
+
+        const food_brands_ids = ["@foodpanda_pk" , "@kfc_pk" , "@Seasonsfoods",
+                                    "@EatMubarak_Pk" , "@SupermealPK" , "@KandNs", 
+                                    "@Foodpakistanpk" , "@Foodnerdpk" , "@cheetaypk"
+                                ];
+        setoptions(food_brands, food_brands_ids);
+        break;
+        /*
+        for (let i = 1; i <= 9; i++) {
+          document.getElementById(i.toString()).innerHTML = food_brands[i-1];
+          document.getElementById(i.toString()).value = food_brands_ids[i-1];
+        }
+        */
+        //document.getElementById("10").style.display = "none";
+        /* 
         document.getElementById("1").innerHTML = "foodpanda";
         document.getElementById("1").value = "@foodpanda_pk";
         document.getElementById("2").innerHTML = "kfcpakistan";
@@ -81,17 +133,29 @@ function hideselectionboxes(){
         document.getElementById("9").value = "@cheetaypk";
         document.getElementById("10").style.display = "none";
         document.getElementById("10").style.display = "none";
-
+        */
         /*
         document.getElementById("9").innerHTML = "";
         document.getElementById("9").value = "";
         document.getElementById("10").innerHTML = "";
         document.getElementById("10").value = "";
         */
-        break;
 
       case "onlineshopping":
-        showselectionboxes(); 
+        showselectionboxes();
+        const shopping_brands = [
+          "Amazon", "AliExpress" , "HomeShopping", "iShoppingpk" , "Daraz" , "Alibaba" ,
+          "Elo" , "Telemart Official" , "Tejar.pk" , "Goto Online Shopping"
+        ]; 
+
+        const shopping_brands_id = [
+          "@amazon" , "@AliExpress_EN" , "@homeshoppingpk" , "@iShoppingpk" , 
+          "@darazpk" , "@AlibabaB2B", "@ExportLeftovers" , "@Telemartstores",
+          "@TejarPK" , "@GotocomPk"
+        ];
+        setoptions(shopping_brands , shopping_brands);
+        break;
+        /*
         document.getElementById("1").innerHTML = "Amazon";
         document.getElementById("1").value = "@amazon";
         document.getElementById("2").innerHTML = "AliExpress";
@@ -113,9 +177,21 @@ function hideselectionboxes(){
         document.getElementById("10").innerHTML = "Goto Online Shopping";
         document.getElementById("10").value = "@GotocomPk";
         break;
-      
+      */
       case "mobilepayment":
         showselectionboxes(); // show selection boxes
+        const mobilepayment = [
+          "JazzCash" ,  "EasyPaisa" , "Zong PayMax" , "UBL Omni" , "Keenu Wallet" , "SimSim" , 
+          "UPaisa" 
+        ];
+
+        const mobilepayment_id = [
+          "@JazzCash" , "@easypaisa" , "@Zongers" , "@UBLDigital" , "@KeenuPak" , "@FINCA_Pakistan" , 
+          "@UpaisaOfficial"
+        ];
+        setoptions(mobilepayment, mobilepayment_id);
+        break;
+        /*
         document.getElementById("1").innerHTML = "JazzCash";
         document.getElementById("1").value = "@JazzCash";
         document.getElementById("2").innerHTML = "EasyPaisa";
@@ -133,6 +209,7 @@ function hideselectionboxes(){
         document.getElementById("8").style.display = "none";
         document.getElementById("9").style.display = "none";
         document.getElementById("10").style.display = "none";
+        */
         /*
         document.getElementById("8").innerHTML = "";
         document.getElementById("8").value = "";
@@ -141,10 +218,21 @@ function hideselectionboxes(){
         document.getElementById("10").innerHTML = "";
         document.getElementById("10").value = "";
         */
-        break;
-
+        //break;
+        
       case "footwear":
-        showselectionboxes(); 
+        showselectionboxes();
+        const footwear = [
+          "Stylo Shoes" , "Unze London (Pakistan)" , "Hush Puppies PK" , "Cat Footwear" , "Borjan Shoes" ,
+          "Metro Shoes Pakistan" , "Bata Pakistan" , "Starlet Shoes"
+        ];
+        const footwear_id = [
+          "@StyloShoesPK" , "@unzepakistan" , "@HushPuppiesPak" , "@CatFootwear" , "@borjanshoes1" ,
+          "@MetroShoesbyNY" , "@BataHomePK" , "@onlinestarlet"
+        ] ;
+        setoptions(footwear, footwear_id);
+        break;
+        /*
         document.getElementById("1").innerHTML = "Stylo Shoes";
         document.getElementById("1").value = "@StyloShoesPK";
         document.getElementById("2").innerHTML = "Unze London (Pakistan)";
@@ -163,16 +251,29 @@ function hideselectionboxes(){
         document.getElementById("8").value = "@onlinestarlet";
         document.getElementById("9").style.display = "none";
         document.getElementById("10").style.display = "none";
+        */
         /*
         document.getElementById("9").innerHTML = "";
         document.getElementById("9").value = "";
         document.getElementById("10").innerHTML = "";
         document.getElementById("10").value = "";
         */
-        break;
+        //break;
 
       case "property":
         showselectionboxes(); // show selection boxes
+        const property = [
+          "Bahria Town Official", "DHA", "Abdullah Group", "Habib Rafiq", "Ali Builders & Developers",
+          "ZEM Builders", "Lahore Development Authority (LDA)."
+        ]; 
+
+        const property_id = [
+          "@BahriaTownOffic", "@DHAToday", "@ABDBuilders", "Habib Rafiq", "@alibuilders", "@zembuilders", 
+          "@LHRDevAuthority"
+        ];
+        setoptions(property, property_id);
+        break;
+        /*
         document.getElementById("1").innerHTML = "Bahria Town Official";
         document.getElementById("1").value = "@BahriaTownOffic";
         document.getElementById("2").innerHTML = "DHA";
@@ -190,6 +291,7 @@ function hideselectionboxes(){
         document.getElementById("8").style.display = "none";
         document.getElementById("9").style.display = "none";
         document.getElementById("10").style.display = "none";
+        */
         /*
         document.getElementById("7").value = ""
         document.getElementById("8").innerHTML = "";
@@ -199,10 +301,22 @@ function hideselectionboxes(){
         document.getElementById("10").innerHTML = "";
         document.getElementById("10").value = "";
         */
-        break;
+        //break;
 
       case "beverage":
         showselectionboxes(); // show selection boxes
+        const beverage = [
+          "Pakola Pakistan", "pepsipakistan", "Coca-Cola Pakistan", "Murree Brewery", "NESTLÉ PURE LIFE",
+          "Mountain Dew", "Sprite", "Fanta", "7UP Pakistan", "Mirinda Pakistan"
+        ];
+
+        const beverage_id = [
+          "@PakolaPakistan", "@pepsipakistan", "@CokePk", "@MurreeBrewery", "@PureLifePK", "@DewPK",
+           "@Sprite", "@fantapakistan", "@7UP_PK", "@MirindaPK"
+        ];
+        setoptions(beverage, beverage_id);
+        break;
+        /*
         document.getElementById("1").innerHTML = "Pakola Pakistan";
         document.getElementById("1").value = "@PakolaPakistan";
         document.getElementById("2").innerHTML = "pepsipakistan";
@@ -223,10 +337,21 @@ function hideselectionboxes(){
         document.getElementById("9").value = "@7UP_PK";
         document.getElementById("10").innerHTML = "Mirinda Pakistan";
         document.getElementById("10").value = "@MirindaPK";
-        break;
+        break;*/
       case "banks":
         showselectionboxes(); // show selection boxes
-
+        const banks = [
+          "SBP", "National Bank Of Pakistan NBP", "Sindh Bank Ltd", "Bank Of Khyber", 
+          "First Women Bank Limited", "Askari Bank Limited", "Bank Alfalah", "MCB Bank Limited", 
+          "The Bank of Punjab"
+        ];
+        const banks_id = [
+          "@StateBank_Pak", "@nationalbankpk", "@SINDHBANKMEDIA", "@TheBankofKhyber", "@FWBLbank", 
+          "@Askari_Bank", "@BankAlfalahPAK", "@MCBBankPk", "@thebankofpunjab"
+        ];
+        setoptions(banks, banks_id);
+        break;
+        /*
         document.getElementById("1").innerHTML = "SBP";
         document.getElementById("1").value = "@StateBank_Pak";
         document.getElementById("2").innerHTML = "National Bank Of Pakistan NBP";
@@ -247,9 +372,18 @@ function hideselectionboxes(){
         document.getElementById("9").value = "@MCBBankPk";
         document.getElementById("10").innerHTML = "The Bank of Punjab";
         document.getElementById("10").value = "@thebankofpunjab";
-        break;
+        break;*/
       case "mobileoperator":
         showselectionboxes(); // show selection boxes
+        const mobileoperator = [
+          "Telenor, Pakistan", "Jazz", "ufone", "Zong", "Warid Telecom" 
+        ];
+        const mobileoperator_id = [
+          "@telenorpakistan", "@jazzpk", "@Ufone", "@Zongers", "@Waridpak"
+        ];
+        setoptions(mobileoperator, mobileoperator_id);
+        break;
+        /*
         document.getElementById("1").innerHTML = "Telenor Pakistan";
         document.getElementById("1").value = "@telenorpakistan";
         document.getElementById("2").innerHTML = "Jazz";
@@ -266,33 +400,35 @@ function hideselectionboxes(){
         document.getElementById("9").style.display = "none";
         document.getElementById("10").style.display = "none";
         break;
-        
+        */
       case "automobiles":
         showselectionboxes(); // show selection boxes
-        document.getElementById("1").innerHTML = "";
-        document.getElementById("1").value = "";
-        document.getElementById("2").innerHTML = "";
-        document.getElementById("2").value = "";
-        document.getElementById("3").innerHTML = "";
-        document.getElementById("3").value = "";
-        document.getElementById("4").innerHTML = "";
-        document.getElementById("4").value = "";
-        document.getElementById("5").innerHTML = "";
-        document.getElementById("5").value = "";
-        document.getElementById("6").innerHTML = "";
-        document.getElementById("6").value = "";
-        document.getElementById("7").innerHTML = "";
-        document.getElementById("7").value = "";
-        document.getElementById("8").innerHTML = "";
-        document.getElementById("8").value = "";
-        document.getElementById("9").innerHTML = "";
-        document.getElementById("9").value = "";
-        document.getElementById("10").innerHTML = "";
-        document.getElementById("10").value = "";
+        const automobiles = [
+          "Toyota Pakistan", "Suzuki Pakistan", "Yamaha Motor Pakistan", "MG Motor Pakistan",
+          "Honda Atlas Cars Pakistan Limited", "Kia Motors Pakistan", "Audi Pakistan", "JAGUAR PAKISTAN LTD.",
+          "Lamborghini", "Mercedes-Benz"
+        ];
+
+        const automobiles_id = [
+          "@ToyotaPak", "@SuzukiPakistan", "@YamahaMotorPk", "@MGPakistan", "@HACPLOfficial", "@KiaMotorsPK",
+          "@AudiPakistan", "@Jaguarpakistan", "@Lamborghini", "@MercedesBenz"
+
+        ];
+        setoptions(automobiles, automobiles_id);
         break;
       
       case "technology":
         showselectionboxes(); // show selection boxes
+        const technology = [
+          "Microsoft", "Amazon Inc", "Alphabet Inc", "Tencent", "Facebook", "Alibaba Group", "Nvidia", "TSMC"
+        ];
+        const technology_id = [ 
+          "@Microsoft", "@amazon", "@Alphabetlnc", "@TencentGlobal", "@Facebook", "@AlibabaGroup", "@nvidia",
+          "@TSMCGlobal" 
+        ];
+        setoptions(technology, technology_id);
+        break;
+        /*
         document.getElementById("1").innerHTML = "Microsoft";
         document.getElementById("1").value = "@Microsoft";
         document.getElementById("2").innerHTML = "Amazon Inc";
@@ -314,7 +450,7 @@ function hideselectionboxes(){
         document.getElementById("10").innerHTML = "";
         document.getElementById("10").value = "";
         break;
-
+        */
       /*case "smartphones":
         showselectionboxes(); // show selection boxes
         document.getElementById("1").innerHTML = "";
